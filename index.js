906fdab3cdc3c2f0589263606a39423c7e790435
ProsteFalco <58610152+ProsteFalco@users.noreply.github.com>
2022-04-14 23:37:13 +0200

    Some small changes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js")
 
 require("dotenv").config()
 
+const prefix = "-"
+
 const client = new Discord.Client({
     intents:[
         "GUILDS",
@@ -14,9 +16,30 @@ client.on("ready", () =>{
 })
 
 client.on("message", (message) =>{
-    if (message.content == "Hi"){
-        message.reply("Hello " + message.author.username + "!")
+    
+    if(!message.content.startsWith(prefix) || message.content.bot) return
+
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase()
+
+    if(command === 'status'){
+        message.channel.send("Fully working!")
     }
+
+    if(command === 'hi'){
+        message.channel.send('Hello!')
+    }
+
+    if(command === 'help'){
+        message.channel.send("I'm here to help!\n\nHere is the list of command you can try: hi, status.")
+    }
+
+    
+    
 })
+
+
+
+
 
 client.login(process.env.TOKEN)
